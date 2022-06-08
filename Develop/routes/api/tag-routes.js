8f152ce65d0ccb10830ea41321ad701fe5420ebe
Damaris Palacios <dslpz728@gmail.com>
2022-06-08 11:05:14 -0500

    starts tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,36 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [{ model: Product, 
+      attributes:['id', 'product_name', 'stock', 'price', 'category_id'],
+    }]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    attributes:['id', 'tag_name'],
+    where: { id: req.params.id, },
+    include: [
+      { model: Product,
+        attributes: ['id', 'product_name', 'stock', 'category_id'],
+     }
+    ]
+  })
+})
+.then(dbTagData => res.json(dbTagData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
 });
 
 router.post('/', (req, res) => {
