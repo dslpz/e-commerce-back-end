533e4266ffa7dd3e61d854149a63009eeed77b7b
Damaris Palacios <dslpz728@gmail.com>
2022-06-08 00:39:27 -0500

    adds to product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      "id",
+      "product_name",
+      "price",
+      "stock",
+      "category_id",
+      "tag_data",
+    ]
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    attributes: [
+      "id",
+      "product_name",
+      "price",
+      "stock",
+      "category_id",
+      "tag_data",
+    ],
+  });
 });
 
 // create new product
